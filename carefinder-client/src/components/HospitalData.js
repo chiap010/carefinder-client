298dc4dfdc9f5a10f89aa766105de0c50e029459
chiap010 <78487437+chiap010@users.noreply.github.com>
2021-09-24 11:13:17 -0500

    Got some hospital data up, finally!!!

--- carefinder-client/src/components/HospitalData.js
+++ carefinder-client/src/components/HospitalData.js
@@ -1,14 +1,15 @@
 import React from 'react'
 
-const HospitalData = ({hospitals}) => {
+const HospitalData = (props) => {
     return (
         <div>
             <center><h1>Hospital List</h1></center>
             
-           { hospitals.map((hospital) =>
-                <h6>{hospital.city}</h6>
-           )}
-               
+            
+            { props.hospitals.data.map( (h) =>
+                    <div> {h.name}</div>
+                    )
+                }
         </div>
     )
 };
