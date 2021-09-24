45c6aa5d2983513de67d6b4eac088b1cfe4a396a
chiap010 <78487437+chiap010@users.noreply.github.com>
2021-09-24 15:37:29 -0500

    Added a home route

---
+++ carefinder-client/src/pages/Home/index.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+
+class Home extends Component {
+
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            
+        }; 
+    }
+
+    render() {
+      return (
+
+        <div >
+        <h1>Home</h1>
+        
+        <ul>
+            <li><a href="/All">All</a></li>
+        </ul>
+         
+        </div>
+
+ 
+      )
+  }
+}
+
+export default Home;
--- carefinder-client/src/routes/index.js
+++ carefinder-client/src/routes/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import All from '../pages/All';
+import Home from '../pages/Home';
 
 export default function Routes() {
     return (
         <Switch>
-            <Route path="/" component={All} />
-            <Route path="/all" component={All} />
-
-            <Route component={All} />
+            <Route path="/All" component={All} />
+            <Route path="/" component={Home} />
+            <Route component={Home} />
         </Switch>
     )
 }
