49c48b2c7e518e654b4cf1c16e12477800be2eff
chiap010 <78487437+chiap010@users.noreply.github.com>
2021-09-23 22:53:41 -0500

    commit

--- carefinder-client/src/App.js
+++ carefinder-client/src/App.js
@@ -1,30 +1,28 @@
 import React, {Component} from 'react';
 import HospitalData from './components/HospitalData';
 
-
 class App extends Component {
-    render() {
-        return (
-            <HospitalData hospitals={this.state.hospitals} />
-
-        )
-    }
-
+    
     state = {
         hospitals: []
     }; 
 
     componentDidMount() {
-        fetch('http://knautzfamilywi.com/CareFinder-1.0.0/api/hospitals/city/Racine', 
-        {
-              headers: { "X-API-KEY": "2e5fc98ee50e2b1c3b8e3ebb40ec476e" }
-        })
+        fetch('http://mango.cs.uwp.edu:3000/api/v1/hospitals/')
+        
             .then(res => res.json())
             .then((data) => {
                 this.setState({ hospitals: data })
             })
             .catch(console.log)
     }
+
+    render() {
+      return (
+          <HospitalData hospitals={this.state.hospitals} />
+
+      )
+  }
 }
 
 export default App;
--- carefinder-client/src/components/HospitalData.js
+++ carefinder-client/src/components/HospitalData.js
@@ -4,15 +4,11 @@ const HospitalData = ({hospitals}) => {
     return (
         <div>
             <center><h1>Hospital List</h1></center>
-            {hospitals.map((hospital) => (
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">{hospital.hopsital_name}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">{hospital.address}</h6>
-                        <p class="card-text">{hospital.city}</p>
-                    </div>
-                </div>
-            ))}
+            
+           { hospitals.map((hospital) =>
+                <h6>{hospital.city}</h6>
+           )}
+               
         </div>
     )
 };
